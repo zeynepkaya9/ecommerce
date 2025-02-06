684426ba950c435ef720f835df666c1d241b8477
zeynepkaya9 <zeynepkaya1723@gmail.com>
2025-02-06 20:16:02 +0300

    EntityDtoMapper updated with usage of mapstruct

--- src/main/java/com/zekaya/eccommerce/mapper/EntityDtoMapper.java
+++ src/main/java/com/zekaya/eccommerce/mapper/EntityDtoMapper.java
@@ -2,106 +2,48 @@ package com.zekaya.eccommerce.mapper;
 
 import com.zekaya.eccommerce.dto.*;
 import com.zekaya.eccommerce.entity.*;
-import org.springframework.stereotype.Component;
+import org.mapstruct.*;
+import org.mapstruct.factory.Mappers;
+import java.util.List;
 
-import java.util.stream.Collectors;
+@Mapper(componentModel = "spring")
+public interface EntityDtoMapper {
+    EntityDtoMapper INSTANCE = Mappers.getMapper(EntityDtoMapper.class);
 
-@Component
-public class EntityDtoMapper {
+    // User Mapping
+    @Mapping(source = "role.name", target = "role")
+    UserDto userToUserDto(User user);
 
-    // map user
-    public UserDto mapUserDtoBasic(User user){
-        UserDto userDto =new UserDto();
-        userDto.setId(user.getId());
-        userDto.setPhoneNumber(user.getPhoneNumber());
-        userDto.setEmail(user.getEmail());
-        userDto.setRole(user.getRole().name());
-        userDto.setName(user.getName());
-        return userDto;
-    }
+    // Address Mapping
+    AddressDto addressToAddressDto(Address address);
 
-    // map address
-    public AddressDto mapAddressToBasic(Address address){
-        AddressDto addressDto = new AddressDto();
-        addressDto.setId(address.getId());
-        addressDto.setCity(address.getCity());
-        addressDto.setState(address.getState());
-        addressDto.setStreet(address.getStreet());
-        addressDto.setCountry(address.getCountry());
-        addressDto.setZipCode(address.getZipCode());
-        return addressDto;
-    }
+    // Category Mapping
+    CategoryDto categoryToCategoryDto(Category category);
 
-    // map category
-    public CategoryDto mapCategoryToDtoBasic(Category category){
-        CategoryDto categoryDto = new CategoryDto();
-        categoryDto.setId(category.getId());
-        categoryDto.setName(category.getName());
-        return categoryDto;
-    }
+    // OrderItem Mapping
+    @Mapping(source = "status.name", target = "status")
+    OrderItemDto orderItemToOrderItemDto(OrderItem orderItem);
 
-    // map orderItem
-    public OrderItemDto mapOrderItemToDtoBasic(OrderItem orderItem){
-        OrderItemDto orderItemDto = new OrderItemDto();
-        orderItemDto.setId(orderItem.getId());
-        orderItemDto.setQuantity(orderItem.getQuantity());
-        orderItemDto.setPrice(orderItem.getPrice());
-        orderItemDto.setStatus(orderItem.getStatus().name());
-        orderItemDto.setCreatedAt(orderItem.getCreatedAt());
-        return orderItemDto;
-    }
+    // Product Mapping
+    ProductDto productToProductDto(Product product);
 
-    //map product
-    public ProductDto mapProductToDtoBasic(Product product){
-        ProductDto productDto = new ProductDto();
-        productDto.setId(product.getId());
-        productDto.setName(product.getName());
-        productDto.setDescription(product.getDescription());
-        productDto.setPrice(product.getPrice());
-        productDto.setImageUrl(product.getImageUrl());
-        return productDto;
-    }
+    // User with Address
+    @Mapping(source = "address", target = "address")
+    UserDto userToUserDtoWithAddress(User user);
 
-    // user plus address
-    public UserDto mapUserDtoPlusAddress(User user){
-        UserDto userDto =mapUserDtoBasic(user);
-        if(user.getAddress() != null){
-            AddressDto addressDto = mapAddressToBasic(user.getAddress());
-            userDto.setAddress(addressDto);
-        }
-        return userDto;
-    }
+    // OrderItem with Product
+    @Mapping(source = "product", target = "product")
+    OrderItemDto orderItemToOrderItemDtoWithProduct(OrderItem orderItem);
 
-    //orderItem plus product
-    public OrderItemDto mapOrderItemToDtoPlusProduct(OrderItem orderItem){
-        OrderItemDto orderItemDto = mapOrderItemToDtoBasic(orderItem);
-        if (orderItem.getProduct()!=null){
-            ProductDto productDto = mapProductToDtoBasic(orderItem.getProduct());
-            orderItemDto.setProduct(productDto);
-        }
-        return orderItemDto;
-    }
+    // OrderItem with Product and User
+    @Mapping(source = "product", target = "product")
+    @Mapping(source = "user", target = "user")
+    OrderItemDto orderItemToOrderItemDtoWithProductAndUser(OrderItem orderItem);
 
-    //orderItem plus product and user
-    public OrderItemDto mapOrderItemToDtoPlusProductAndUser(OrderItem orderItem){
-        OrderItemDto orderItemDto = mapOrderItemToDtoPlusProduct(orderItem);
-        if(orderItem.getUser() != null){
-            UserDto userDto = mapUserDtoPlusAddress(orderItem.getUser());
-            orderItemDto.setUser(userDto);
-        }
-        return orderItemDto;
-    }
+    // User with Address and Order History
+    @Mapping(source = "address", target = "address")
+    @Mapping(source = "orderItemList", target = "orderItemList")
+    UserDto userToUserDtoWithAddressAndOrderHistory(User user);
 
-    //User plus Address and Order Item History
-    public UserDto mapUserToDtoPlusAddressAndOrderHistory(User user){
-        UserDto userDto =mapUserDtoPlusAddress(user);
-        if(user.getOrderItemList() != null && !user.getOrderItemList().isEmpty()){
-            userDto.setOrderItemList(user.getOrderItemList()
-                    .stream()
-                    .map(this::mapOrderItemToDtoPlusProduct)
-                    .collect(Collectors.toList()));
-
-        }
-        return userDto;
-    }
+    List<OrderItemDto> orderItemsToOrderItemDtos(List<OrderItem> orderItems);
 }
