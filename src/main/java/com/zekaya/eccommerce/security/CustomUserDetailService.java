d660de66344f2efb78f488ec40df6f3f3b7c8074
zeynepkaya9 <zeynepkaya1723@gmail.com>
2025-02-17 17:21:49 +0300

    AuthUser created

---
+++ src/main/java/com/zekaya/eccommerce/security/CustomUserDetailService.java
@@ -0,0 +1,28 @@
+package com.zekaya.eccommerce.security;
+
+import com.zekaya.eccommerce.entity.User;
+import com.zekaya.eccommerce.exception.NotFoundException;
+import com.zekaya.eccommerce.repository.UserRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepo userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepo.findByEmail(username)
+                .orElseThrow(()-> new NotFoundException("User / Email not found"));
+
+        return AuthUser.builder()
+                .user(user)
+                .build();
+    }
+}
