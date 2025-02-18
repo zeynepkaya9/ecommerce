e20699fc6447144c05336ef087d9ab2f0508485c
zeynepkaya9 <zeynepkaya1723@gmail.com>
2025-02-18 12:29:30 +0300

    JwtUtils created for token generation and validation

---
+++ src/main/java/com/zekaya/eccommerce/security/JwtUtils.java
@@ -0,0 +1,66 @@
+package com.zekaya.eccommerce.security;
+
+
+import com.zekaya.eccommerce.entity.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+@Slf4j
+public class JwtUtils {
+
+    private static final long EXPIRATION_TIME_IN_MILLISEC = 1000L * 60L * 24L * 30L * 6L; // expires after 6 months
+
+    private SecretKey key;
+    @Value("${secretJwtString}")
+    private String secretJwtString; // the value in application properties should longer than 32characters
+
+    @PostConstruct
+    private void init(){
+        byte[] keyBytes = secretJwtString.getBytes(StandardCharsets.UTF_8);
+        this.key = new SecretKeySpec(keyBytes, "HmacSHA256");
+    }
+
+    public String generateToken(User user){
+        String username= user.getEmail();
+        return generateToken(username);
+    }
+
+    public String generateToken(String username){
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME_IN_MILLISEC))
+                .signWith(key)
+                .compact();
+    }
+
+    public String getUsernameFromToken(String token){
+        return extractClaims(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaims(String token, Function<Claims, T> claimsTFunction){
+        return claimsTFunction.apply(Jwts.parser().verifyWith(key).build().parseSignedClaims(token).getPayload());
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = getUsernameFromToken(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+
+    }
+
+    private boolean isTokenExpired(String token){
+        return extractClaims(token, Claims::getExpiration).before(new Date());
+    }
+}
