f02f9fd6452a85996001c205ec78cff537faf725
zeynepkaya9 <zeynepkaya1723@gmail.com>
2025-04-28 23:51:50 +0300

    AWS S3 implemented AWSS3Service created

---
+++ src/main/java/com/zekaya/eccommerce/service/AwsS3Service.java
@@ -0,0 +1,60 @@
+package com.zekaya.eccommerce.service;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Service
+@Slf4j
+public class AwsS3Service {
+
+    private final String bucketName = "zekaya-ecom";
+
+    @Value("${aws.s3.access}")
+    private String awsS3AccessKey;
+
+    @Value("${aws.s3.secrete}")
+    private String awsS3SecreteKey;
+
+    public String saveImageToS3(MultipartFile photo){
+        try {
+           String s3FileName = photo.getOriginalFilename();
+           //create aws credentials using the access and service key
+            BasicAWSCredentials awsCredentials = new BasicAWSCredentials(awsS3AccessKey, awsS3SecreteKey);
+
+            //create an s3 client with config credentials and region
+            AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                    .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                    .withRegion(Regions.EU_NORTH_1)
+                    .build();
+
+            //get input stream from photo
+            InputStream inputStream = photo.getInputStream();
+
+            //set metadata for the object
+            ObjectMetadata metadata =new ObjectMetadata();
+            metadata.setContentType("image/jpeg");
+
+            //create a put request to upload the image to s3
+            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, s3FileName, inputStream, metadata);
+            s3Client.putObject(putObjectRequest);
+
+            return "https://" + bucketName + ".s3.eu-north-1.amazonaws.com/" +s3FileName;
+
+        }catch (IOException e){
+            e.printStackTrace();
+            throw new RuntimeException("Unable to upload image to s3bucket: " +e.getMessage());
+        }
+    }
+}
